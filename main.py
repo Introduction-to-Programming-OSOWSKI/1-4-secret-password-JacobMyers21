e8fbc560f9f3fe7462d66423432105f119e7852e
Myers Jacob <813862@stma.k12.mn.us>
2021-03-29 09:59:05 -0500

    not finished yet. Just being safe

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #WRITE YOUR CODE IN THIS FILE
+def password(p):
+    return p
+    if p = "Knights19"
+        print("Good job!")
+    else
+        print("HACKER ALERT! HACKER ALERT")
+print(password(Knights19):)
